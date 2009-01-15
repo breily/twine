9e81fa3bd8dd48a6659d9a669ffe9bef5dfc42e8
Brian Reily <breily@gmail.com>
2009-01-15 01:44:15 -0500

    added readme stuff, plus commented example programs

--- simplemutex.c
+++ simplemutex.c
@@ -1,39 +1,51 @@
 #include <stdio.h>
 #include "thread.h"
 
+// Our mutex lock
 twine_mutex mutex;
-int mutex_test = 1123;
+// A value we will use to test the mutex locks
+int mutex_test = 7;
+// Function to create threads
 void foo(int th);
 
 int main() {
+    // Initialize Twine
     twine_init();
+    // Initialize our mutex lock
     twine_mutex_init(&mutex);
+    // Create and enqueue two threads
     twine_create(foo, 1);
     twine_create(foo, 2);
 
-    int backup;
     int i;
     for (i = 0; i < 5; i++) {
+        // Lock mutex - Atomic operation
         twine_mutex_lock(&mutex);
         printf("** thread 0 working...\n");
         mutex_test = 0;
+        // Yield to another thread
         twine_yield();
+        // Lock shoud have prevented changes
         if (mutex_test != 0) printf("!! thread 0 mutex failed\n");
         else printf("** thread 0 mutex passed\n");
+        // Unlock the mutex
         twine_mutex_unlock(&mutex);
     }
 }
 
 void foo(int th) {
-    int backup;
     int i;
     for (i = 0; i < 5; i++) {
+        // Lock mutex
         twine_mutex_lock(&mutex);
         printf("** thread %d working...\n", th);
         mutex_test = th;
+        // Yield to another thread
         twine_yield();
+        // Hopefully no change
         if (mutex_test != th) printf("!! thread %d mutex failed\n", th);
         else printf("** thread %d mutex passed\n", th);
+        // Unlock mutex
         twine_mutex_unlock(&mutex);
     }
 }
--- simplethreads.c
+++ simplethreads.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include "thread.h"
 
+// Our function that we'll create threads of
 void foo(int th);
 
 int main() {
+    // Initialize Twine
     twine_init();
+
+    // Create two threads and adds them to the queue
     twine_create(foo, 1);
     twine_create(foo, 2);
 
     int i;
     for (i = 0; i < 5; i++) {
+        // main thread is thread 0
         printf("** thread 0 working...\n");
+        // Yields to the next thread in the queue
         twine_yield();
     }
 }
@@ -19,6 +25,7 @@ void foo(int th) {
     int i;
     for (i = 0; i < 5; i++) {
         printf("** thread %d working...\n", th);
+        // Yields to the next thread in the queue
         twine_yield();
     }
 }
